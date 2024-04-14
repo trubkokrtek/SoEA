49f69929baf3915af13fd0f87018bc269955a08c
trubkokrtek <agentdominik707@gmail.com>
2024-04-14 19:42:07 +0200

    Create simulator_of_epic_adventure.py
    added src

---
+++ simulator_of_epic_adventure.py
@@ -0,0 +1,9 @@
+pass
+#TODO 5 artefaktů k otevření petrovy kapsy, k získání karty ke kouzelným dveřím. Otevírání ve tvaru pentagramu. 5 PC (každému chybí něco jiného)
+#TODO Artefakty: Honzův kabel, instalační soubory LabView, Pepovu raketu (a naistalovat naní win11), pojistku, přihlašovací údaje k novým PC na TULce
+#TODO good endings: probuzení ze snu
+#TODO bad endings: přejetí vlakem po vyjití z budovy
+#TODO items: Kafe / Honzovo kafe (nicked), tvoje zapomenuté USB
+#TODO LV: nasel jsi svoje stare USB se soubory (clickbait - corrupted)
+print("""Ahoj, právě ses probudil na TULce. Nevíš jak jsi se sem dostal, ale vedle tebe na zemi je nakresleno něco jako pentagram a v každém kruhu je počítač, kromě jednoho""")
+print("Co udeláš? Utečeš, nebo se na to podíváš zblízka")
