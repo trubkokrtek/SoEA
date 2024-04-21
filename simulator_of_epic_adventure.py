1c752920a440cd771163d50dff4bac6a729bc2d6
trubkokrtek <agentdominik707@gmail.com>
2024-04-21 13:55:46 +0200

    graphics rework

--- simulator_of_epic_adventure.py
+++ simulator_of_epic_adventure.py
@@ -4,6 +4,24 @@ import time as t
 os.system("Color a")
 os.system("CLS")
 
+def gameover():
+    #? gameover_theme = pd.AudioSegment.from_mp3("")
+    print(""" 
+    ######################################################
+    #                                                    #
+    #                                                    #
+    #                                                    #
+    #   ____                                             #
+    #  / ___| __ _ _ __ ___   ___    _____   _____ _ __  #
+    # | |  _ / _` | '_ ` _ \ / _ \  / _ \ \ / / _ \ '__| #
+    # | |_| | (_| | | | | | |  __/ | (_) \ V /  __/ |    #
+    #  \____|\__,_|_| |_| |_|\___|  \___/ \_/ \___|_|    #
+    #                                                    #
+    #                                                    #  
+    #                                                    #
+    ######################################################
+    """)
+
 #? music department
 #? scary_sound = pd.AudioSegment.from_mp3("./scary.mp3")
 
@@ -14,7 +32,29 @@ os.system("CLS")
 #TODO items: Kafe / Honzovo kafe (nicked), tvoje zapomenuté USB
 #TODO LV: nasel jsi svoje stare USB se soubory (clickbait - corrupted)
 #TODO easter eggs: JS, SolidWorks
-#? add music
+
+print("""
+ _____           _     _         _         _       _    
+|_   _| __ _   _| |__ | | _____ | | ___ __| |_ ___| | __
+  | || '__| | | | '_ \| |/ / _ \| |/ / '__| __/ _ \ |/ /
+  | || |  | |_| | |_) |   < (_) |   <| |  | ||  __/   < 
+  |_||_|   \__,_|_.__/|_|\_\___/|_|\_\_|   \__\___|_|\_\ """)
+t.sleep(5)
+print("""
+                                _                       
+ _ __  _ __ ___  ___  ___ _ __ | |_ ___                 
+| '_ \| '__/ _ \/ __|/ _ \ '_ \| __/ __|                
+| |_) | | |  __/\__ \  __/ | | | |_\__ \_ _ _           
+| .__/|_|  \___||___/\___|_| |_|\__|___(_|_|_)          
+|_|       """)
+t.sleep(5)
+print("""
+ ____        _____    _        _____ _   _ _            
+/ ___|  ___ | ____|  / \   _  |_   _| | | | |           
+\___ \ / _ \|  _|   / _ \ (_)   | | | | | | |           
+ ___) | (_) | |___ / ___ \ _    | | | |_| | |___        
+|____/ \___/|_____/_/   \_(_)   |_|  \___/|_____|""")
+
 print("""Ahoj, právě ses probudil na TULce.
       Nevíš jak jsi se sem dostal, ale vedle tebe na zemi je nakresleno něco jako pentagram a v každém kruhu je počítač, kromě jednoho""")
 #? pd.play(scary_sound)
@@ -22,7 +62,7 @@ print("""Co udeláš? Utečeš, nebo se na to podíváš zblízka:
         1 - Uteču
         2 - Podívám se na ten pentagram zblízka""")
 answer = input("Co tedy uděláš? 1 / 2: ")
-if answer == 1:
+if answer == '1':
     print("Vyběhneš z místnosti s pentagramem k nelblišším dveřím, ale zjistíš, že dveře jsou zamčené.")
     print("Podíváš se blíže na zámek dveří a zjistíš, že k jejich otevření je potřeba legendární kouzelná karta, kterou lze nalést jen na jediném místě.")
     #? pd.play(dark_sourprise_sound)
@@ -41,7 +81,7 @@ if answer == 1:
     print("Užij si hru!")
     #? pd.play(start_game)
 
-elif answer == 2:
+elif answer == '2':
     #? pd.play(brave_music)
     print("Zjistíš, že uprostřed pentagramu se nachází trezor s nápisem \"Petrova kapsa\"")
     #? pd.play(inteligent_music)
@@ -64,3 +104,4 @@ else:
     #? pd.play(game_over_theme)
     print("Kdybys nenapsal nesmyslnou odpověď na první volbu, nic by se ti nestalo")
     print("Umřel jsi a to hra ještě nazačala, tys to vyved!")
+    gameover()
